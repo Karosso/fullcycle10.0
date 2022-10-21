fe219d6f1399ab5b9c1298e25f4937a6bb79c968
oscar dias <oscar.dias@meep-app.com>
2022-10-20 21:03:44 -0300

    Criado microserviço nestjs e integrado com apache kafka

---
+++ nest-api/src/routes/routes.controller.ts
@@ -0,0 +1,51 @@
+import { Controller, Get, Inject, OnModuleInit, Param } from '@nestjs/common';
+import { ClientKafka, MessagePattern, Payload } from '@nestjs/microservices';
+import { Producer } from '@nestjs/microservices/external/kafka.interface';
+import { RoutesService } from './routes.service';
+
+@Controller('routes')
+export class RoutesController implements OnModuleInit {
+  private kafkaProducer: Producer;
+
+  constructor(
+    private readonly routesService: RoutesService,
+    @Inject('KAFKA_SERVICE')
+    private kafkaClient: ClientKafka,
+  ) {}
+
+  async onModuleInit() {
+    this.kafkaProducer = await this.kafkaClient.connect();
+  }
+
+  @Get()
+  findAll() {
+    return this.routesService.findAll();
+  }
+
+  @Get(':id/start')
+  startRoute(@Param('id') id: string) {
+    console.log('Start route: ', id);
+    this.kafkaProducer.send({
+      topic: 'route.new-direction',
+      messages: [
+        {
+          key: 'route.new-direction',
+          value: JSON.stringify({ routeId: id, clientId: '' }),
+        },
+      ],
+    });
+  }
+
+  @MessagePattern('route.new-position')
+  consumeNewPosition(
+    @Payload()
+    message: {
+      routeId: string;
+      clientId: string;
+      position: [number, number];
+      finished: boolean;
+    },
+  ) {
+    console.log({ message });
+  }
+}
